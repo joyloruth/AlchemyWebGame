41283f9b4d4084a3f1605c0f13bc0e2e2d9763a1
joyloruth <joyloruth@gmail.com>
2022-02-16 23:37:33 -0500

    adding additional if statements

--- StartGame/startGame.js
+++ StartGame/startGame.js
@@ -17,43 +17,20 @@ const move = function(element){
 
         chooseElement.style.left = x + "px";
         chooseElement.style.top  = y + "px";
-         
-        
 
-        
-        //create steam when water and fire are combined
-        if (fire.style.top === water.style.top || fire.style.left === water.style.left){
-          let steam = document.getElementById("steam");
-          steam.style.display = "flex";
-          let contain = document.getElementById("container");
-          contain.appendChild(steam);
-          contain.style.position ="absolute";
-          let alert = document.createElement('div');
-          
-          alert.innerHTML = "steam found";
-          
-          alert.setAttribute('class', 'alertNote');
-          contain.appendChild(alert);
 
-        } else if (fire.style.top === earth.style.top || fire.style.left === earth.style.left ){
-          let lava = document.getElementById("lava");
-          lava.style.display = "flex";
-          let contain = document.getElementById("container");
-          contain.appendChild(lava);
-          contain.style.position ="absolute";
-        } else if(earth.style.top === water.style.top || earth.style.left === water.style.left){
-          let mud = document.getElementById("mud");
-          mud.style.display = "flex";
-          let contain = document.getElementById("container");
-          contain.appendChild(mud);
-          contain.style.position ="absolute";
-        } else if(fire.style.top === air.style.top || fire.style.left === air.style.left){
+        waterMix();
+        earthMix();
+        
+        /*
+        
+        }/* if(fire.style.top === air.style.top || fire.style.left === air.style.left){
           let smoke = document.getElementById("smoke");
           smoke.style.display = "flex";
           let contain = document.getElementById("container");
           contain.appendChild(smoke);
           contain.style.position ="absolute";
-        } else if(mud.style.top === fire.style.top || mud.style.left === fire.style.left){
+        } if(mud.style.top === fire.style.top || mud.style.left === fire.style.left){
           let brick = document.getElementById("brick");
           brick.style.display = "flex";
           let contain = document.getElementById("container");
@@ -61,7 +38,7 @@ const move = function(element){
           contain.style.position ="absolute";
         } else{
         console.log("nothing");
-      }
+      }*/
         
       }
     })
@@ -102,28 +79,41 @@ const moveMixture = function(mixture){
     alert.style.display ="none";
   }
 
+  function waterMix(){
+    //create steam when water and fire are combined
+    if(fire.style.top === water.style.top || fire.style.left === water.style.left){
+      let steam = document.getElementById("steam");
+      steam.style.display = "flex";
+      let contain = document.getElementById("container");
+      contain.appendChild(steam);
+      contain.style.position ="absolute";
+    }
+    if(fire.style.top === earth.style.top || fire.style.left === earth.style.left ){
+      let lava = document.getElementById("lava");
+      lava.style.display = "flex";
+      let contain = document.getElementById("container");
+      contain.appendChild(lava);
+      contain.style.position ="absolute";
+    }
+  }
 
-  // let lava = document.createElement('div');
-          // lava.setAttribute('class', 'element');
-          // lava.style.backgroundColor = "red";
-          // lava.style.width = 300;
-          // lava.style.height = 300;
-          // lava.innerHTML = "lava";
-          // let contain = document.getElementById("container");
-          
-          // contain.appendChild(lava);
-          // contain.style.position ="absolute";
-
-
- //  if(fire.left > water.left + water.width ||
- //     fire.left + fire.width < water.left  ||
- //     fire.top > water.top + water.height||
- //     fire.top + fire.height < water.top
- //     ){
- //       console.log("combined");
-        //  }
+  function earthMix(){
+    if(fire.style.top === air.style.top || fire.style.left === air.style.left){
+        let smoke = document.getElementById("smoke");
+        smoke.style.display = "flex";
+        let contain = document.getElementById("container");
+        contain.appendChild(smoke);
+        contain.style.position ="absolute";
+      } 
       
-        //  else{
-        //    console.log("not combined" + " " + element);
-      
-        //  }
+    if(mud.style.top === fire.style.top || mud.style.left === fire.style.left){
+        let brick = document.getElementById("brick");
+        brick.style.display = "flex";
+        let contain = document.getElementById("container");
+        contain.appendChild(brick);
+        contain.style.position ="absolute";
+      }
+    }
+  
+  
+  
